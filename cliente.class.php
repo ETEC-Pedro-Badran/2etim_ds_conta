c63c94fbd91f13913f52b00c1a660799a518199a
evaldofreu <evaldo.junior@etec.sp.gov.br>
2022-04-14 11:01:27 -0300

    Implementação de objetos na index.

--- cliente.class.php
+++ cliente.class.php
@@ -1,17 +1,35 @@
 <?php
 class Cliente {
     private $id;
+    private $bloqueado;
+    private $motivoBloqueio;
+    private $nome;
+    private $rm;
+    private $classe;
 
+ 
 
-    function __construct($id)
+    function __construct($id, $bloqueado, $motivoBloqueio, $nome, $rm, $classe)
     {
+
+        $this->id             = $id;
+        $this->bloqueado      = $bloqueado;
+        $this->motivoBloqueio = $motivoBloqueio;
+        $this->nome           = $nome;
+        $this->rm             = $rm;
+        $this->classe         = $classe;
         
     }
 
-    
+    function negativar($motivo){
+    }
 
-    function incluir(){
-      ////
+    function incluir(){}
+
+    function modificar(){}
+
+    function getNome(){
+        return $this->nome;
     }
 
 }
